afff5e4facab1d64502b300e8f55bf23d86edef1
Hyunsik Choi <hyunsik.choi@gmail.com>
2016-12-15 23:24:36 +0900

    Add accept method to Actor trait.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate libc;
 extern crate crossbeam;
 extern crate env_logger;
 #[macro_use] extern crate log;
+extern crate rustc_serialize;
 
 pub mod ascii;
 pub mod err;
--- src/reactive/mod.rs
+++ src/reactive/mod.rs
@@ -3,26 +3,27 @@ use std::thread::{self, JoinHandle};
 use std::time::Duration;
 use crossbeam::sync::MsQueue;
 use env_logger;
+use rustc_serialize::Decodable;
 
-pub struct EventDispatcher<M: 'static + Sync + Send, E: 'static + Sized + Sync + Send> {
+pub struct EventBus<M: 'static + Sync + Send + Decodable, E: 'static + Sized + Sync + Send> {
   actors: Arc<Mutex<Vec<Box<Actor<M, E>>>>>,
   queue: Arc<MsQueue<M>>,
   stopped: Arc<Mutex<bool>>,
   thread: JoinHandle<Result<(), E>>,
 }
 
-unsafe impl<M: 'static + Sync + Send, E: 'static + Sized + Sync + Send> Sync for EventDispatcher<M, E> {}
-unsafe impl<M: 'static + Sync + Send, E: 'static + Sized + Sync + Send> Send for EventDispatcher<M, E> {}
+unsafe impl<M: 'static + Sync + Send + Decodable, E: 'static + Sized + Sync + Send> Sync for EventBus<M, E> {}
+unsafe impl<M: 'static + Sync + Send + Decodable, E: 'static + Sized + Sync + Send> Send for EventBus<M, E> {}
 
-impl<M: Sync + Send, E: Sized + Sync + Send> EventDispatcher<M, E> {
+impl<M: Sync + Send + Decodable, E: Sized + Sync + Send> EventBus<M, E> {
 
-  pub fn new(actors: Vec<Box<Actor<M,E>>>) -> EventDispatcher<M, E> {
+  pub fn new(actors: Vec<Box<Actor<M,E>>>) -> EventBus<M, E> {
     let actors = Arc::new(Mutex::new(actors));
     let queue = Arc::new(MsQueue::new());
     let stopped = Arc::new(Mutex::new(false));
     let sleep_time = Duration::from_millis(50);
 
-    EventDispatcher {
+    EventBus {
       actors: actors.clone(),
       queue: queue.clone(),
       stopped: stopped.clone(),
@@ -48,23 +49,21 @@ impl<M: Sync + Send, E: Sized + Sync + Send> EventDispatcher<M, E> {
 
 pub fn run<M, E>(stop: Arc<Mutex<bool>>, queue: Arc<MsQueue<M>>,
     actors: Arc<Mutex<Vec<Box<Actor<M, E>>>>>) -> JoinHandle<Result<(), E>>
-    where M: 'static + Send + Sync, E: 'static + Sync + Send {
+    where M: 'static + Send + Sync + Decodable, E: 'static + Sync + Send {
 
-  debug!("enter run");
-  let sleep_time = Duration::from_millis(500);
+  let sleep_time = Duration::from_millis(50);
+
   thread::spawn(move || -> Result<(), E> {
+     
      loop {
-       debug!("loop start");
         if let Some(m) = queue.try_pop() {
-          debug!("message exists");
-          for actor in actors.lock().unwrap().iter_mut() {
+          for actor in actors.lock().unwrap().iter_mut().filter(|x| x.accept(&m)) {
             match actor.on_receive(&m) {
               Err(e) => return Err(e),
               _ => {}
             }
           }
        } else {
-         debug!("sleeped");
          thread::sleep(sleep_time);
         }
 
@@ -72,55 +71,15 @@ pub fn run<M, E>(stop: Arc<Mutex<bool>>, queue: Arc<MsQueue<M>>,
           break;
         }
       }
-      debug!("escaped from loop");
+
       Ok(())
   })
 }
 
-pub trait Actor<M: Sync + Send, E: Sized + Send + Sync>: Send + Sync {
+pub trait Actor<M: Sync + Send + Decodable, E: Sized + Send + Sync>: Send + Sync {
+  fn accept(&self, m: &M) -> bool { true }
   fn on_receive(&mut self, m: &M) -> Result<(), E>;
 }
-
-pub enum Message {
-  Ask(String),
-  Others
-}
-
-impl Message {
-  pub fn ask(message: &str) -> Message {
-    Message::Ask(message.to_owned())
-  }
-}
-
-unsafe impl Send for Message {}
-unsafe impl Sync for Message {}
-
-pub struct TestActor;
-
-pub enum ActorErr {
-  Err(String)
-}
-
-unsafe impl Send for ActorErr {}
-unsafe impl Sync for ActorErr {}
-
-impl Actor<Message, ActorErr> for TestActor {
-  fn on_receive(&mut self, m: &Message) -> Result<(), ActorErr> {
-    match *m {
-      Message::Ask(ref s) => {
-        debug!(">>> received: {}", s);
-        Ok(())
-      }
-      _ => panic!("unknown message")
-    }
-  }
-}
-
-// impl<Message, ActorErr> Actor<Message, ActorErr> for TestActor {
-//   fn on_receive(&mut self, m: &String) -> Result<(), ActorErr> {
-//     Ok(())
-//   }
-// }
 
 #[cfg(test)]
 mod tests {
@@ -130,12 +89,48 @@ mod tests {
   use env_logger;
   use super::*;
 
+  #[derive(RustcDecodable, RustcEncodable)]
+  pub enum Message {
+    Ask(String),
+    Others
+  }
+
+  impl Message {
+    pub fn ask(message: &str) -> Message {
+      Message::Ask(message.to_owned())
+    }
+  }
+
+  unsafe impl Send for Message {}
+  unsafe impl Sync for Message {}
+
+  pub struct TestActor;
+
+  pub enum ActorErr {
+    Err(String)
+  }
+
+  unsafe impl Send for ActorErr {}
+  unsafe impl Sync for ActorErr {}
+
+  impl Actor<Message, ActorErr> for TestActor {
+    fn on_receive(&mut self, m: &Message) -> Result<(), ActorErr> {
+      match *m {
+        Message::Ask(ref s) => {
+          debug!(">>> received: {}", s);
+          Ok(())
+        }
+        _ => panic!("unknown message")
+      }
+    }
+  }
+
   #[test]
   fn test() {
     env_logger::init().unwrap();
 
     let actors: Vec<Box<Actor<Message, ActorErr>>> = vec![Box::new(TestActor)];
-    let mut dispatcher = EventDispatcher::new(actors);
+    let mut dispatcher = EventBus::new(actors);
 
     let time = Duration::from_secs(5);
     dispatcher.send(Message::Ask("abc".to_owned()));
