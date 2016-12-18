824f7e607622c87c5349eca0596b8482bf4c97c2
Hyunsik Choi <hyunsik.choi@gmail.com>
2016-12-18 14:48:24 +0900

    Extract tests of actor module into another module

--- src/react/mod.rs
+++ src/react/mod.rs
@@ -12,6 +12,8 @@ use rustc_serialize::Decodable;
 
 use self::dispatcher::{Dispatcher, AsyncDispatcher};
 
+pub use self::actor::Actor;
+
 pub trait MsgTrait: 'static + Sync + Send + Decodable {}
 pub trait Error: 'static + Sized + Sync + Send {}
 
@@ -28,68 +30,5 @@ impl<M: MsgTrait, E: Error> ActorSystem<M, E> {
 
   pub fn dispatcher(&self) -> Arc<Box<Dispatcher<M, E>>> {
     self.dispatcher.clone()
-  }
-}
-
-#[cfg(test)]
-mod tests {
-  use std::thread;
-  use std::time::Duration;
-
-  use env_logger;
-  use super::*;
-
-  #[derive(RustcDecodable, RustcEncodable)]
-  pub enum Message {
-    Ask(String),
-    Others
-  }
-
-  impl Message {
-    pub fn ask(message: &str) -> Message {
-      Message::Ask(message.to_owned())
-    }
-  }
-
-  impl MsgTrait for Message {}
-  unsafe impl Send for Message {}
-  unsafe impl Sync for Message {}
-
-  pub struct TestActor;
-
-  pub enum ActorErr {
-    Err(String)
-  }
-  impl Error for ActorErr {}
-  unsafe impl Send for ActorErr {}
-  unsafe impl Sync for ActorErr {}
-
-  impl Actor<Message, ActorErr> for TestActor {
-    fn on_receive(&mut self, m: &Message) -> Result<(), ActorErr> {
-      match *m {
-        Message::Ask(ref s) => {
-          debug!(">>> received: {}", s);
-          Ok(())
-        }
-        _ => panic!("unknown message")
-      }
-    }
-  }
-
-  #[test]
-  fn test() {
-    env_logger::init().unwrap();
-
-    let actors: Vec<Box<Actor<Message, ActorErr>>> = vec![Box::new(TestActor)];
-    let mut dispatcher = Dispatcher::new(actors);
-
-    let time = Duration::from_secs(5);
-    dispatcher.send(Message::Ask("abc".to_owned()));
-    thread::sleep(time);
-
-    debug!("after sleep");
-    dispatcher.stop();
-    debug!("after stop");
-    dispatcher.join().ok().unwrap();
   }
 }
